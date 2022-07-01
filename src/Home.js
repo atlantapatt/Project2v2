305f12677d1aa1ddbce52bcfc2abb00d28cdd343
atlantapatt <99867479+atlantapatt@users.noreply.github.com>
2022-07-01 15:06:51 -0700

    Add JSON post

--- src/Home.js
+++ src/Home.js
@@ -1,35 +1,50 @@
-import { useEffect, useState } from "react";
-import Feed from "./HomeComponents/Feed";
-import Leftbar from "./HomeComponents/Leftbar";
-import Navbar from "./HomeComponents/Navbar";
-import Post from "./HomeComponents/Post";
-import Rightbar from "./HomeComponents/Rightbar";
-import "/home/atlantapatt/Development/code/phase-2/project/src/CSS/Home.css"
+import React, { useEffect, useState } from "react";
+import { Redirect } from "react-router-dom";
+import Feed from "./HomeComonents/Feed";
+import Leftbar from "./HomeComonents/Leftbar";
+import Navbar from "./HomeComonents/Navbar";
+import Post from "./HomeComonents/Post";
+import Rightbar from "./HomeComonents/Rightbar";
+import "/home/atlantapatt/Development/code/phase-2/projectv2/src/CSS/Home.css"
 
-function Home({setIsLoggedIn, isLoggedIn, loginData, setLoginData}) {
+function Home({isLoggedIn, loginData, setLoginData}) {
+    const startValue = []
     const [post, setPost] = useState(false)
-    const [search, setSearch] = useState('')
-    const [postData, setPostData] = useState([])
+    const [search, setSearch] = useState("")
+    const [postData, setPostData] = useState([]);
     console.log(postData)
-    useEffect(() => {
-        fetch('http://localhost:3000/users')
-        .then((r) => r.json())
-        .then((data) => setPostData(data))
-    },[])
+    // useEffect (() => {
+    //    fetch('http://localhost:3000/users')
+    //     .then((r) => r.json())
+    //     .then((data) => {
+    //         console.log(data)
+    //     })
+    // },[])
+
+ useEffect(() => {
+    fetch("http://localhost:3000/users")
+    .then((r) => r.json())
+    .then((data) => setPostData(data))
+ },[])
 
     function addNewPost(post) {
-        setPostData([...postData, post])
+        setPostData(...postData, post)
     }
-    return ( 
+    console.log(postData)
+
+if (isLoggedIn === false) return <Redirect to="/login" />
+
+    return(
         <div>
             <Navbar setSearch={setSearch} setPost={setPost}/>
             <div className="home-container">
+
                 <Leftbar postData={postData} setPost={setPost} />
-               {post === false ? <Feed postData={postData} search={search} /> : <Post loginData={loginData} setLoginData={setLoginData} addNewPost={addNewPost}/>}
+               {post === false ? <Feed postData={postData} search={search} /> : <Post loginData={loginData} setLoginData={setLoginData} addNewPost={addNewPost} setPost={setPost}/>}
                 <Rightbar postData={postData}/>
             </div>
         </div>
-     );
+    )
 }
 
-export default Home;
+export default Home
