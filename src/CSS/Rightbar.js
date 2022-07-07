6ce152eac9f62ac20623c6b87eea57ac931adc4d
atlantapatt <99867479+atlantapatt@users.noreply.github.com>
2022-07-07 16:57:22 -0700

    remove border

---
+++ src/CSS/Rightbar.js
@@ -0,0 +1,27 @@
+import "/home/atlantapatt/Development/code/phase-2/projectv2/src/CSS/Rightbar.css"
+
+function Rightbar({postData}) {
+    const onlineData = postData.filter(user => {
+        if (user.online === true) {
+            return user
+        }
+    })
+    console.log(onlineData)
+    return ( 
+        <div className="rightbar">
+            <div className="rightbar-wrap">
+                <h3 className="online-title">Online Friends</h3>
+                <hr></hr>
+                {onlineData.map((data) => (
+                    <div className="online-friend-list">
+                        <img className="online-image" src={data.avatar}></img>
+                        <span className="online-dot">""</span>
+                        <p className="online-name">{data.username}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+     );
+}
+
+export default Rightbar;
